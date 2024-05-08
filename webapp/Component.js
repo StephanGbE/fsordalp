08a81a58ef1b50ade812e76d9097405e2c30a6d8
StephanGbE <stephan.glas@bearingpoint.com>
2024-05-08 13:12:37 +0200

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("at.be.rapsalesorder.fsordalp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
